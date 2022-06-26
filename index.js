c86428afc2af488fdb4299924b27b4b2ccbd37b1
george kinyanjui1 <george.kinyajui@student.moringaschool.com>
2022-06-26 23:00:17 +0300

    add a function

--- index.js
+++ index.js
@@ -26,3 +26,35 @@ form.addEventListener("submit", (e) => {
 
     }
 })
+
+let comp = document.getElementById("comp-info");
+let close = false
+
+comp.addEventListener("click", (e) => {
+    e.preventDefault
+    fetch("https://georgekinyanjui1.github.io/API/db.json")
+        .then((response) => response.json())
+        .then((data) => {
+            let info2 = document.createElement("li")
+            info2.innerText = data.Transport[1].company
+            comp.appendChild(info2)
+            let info = document.createElement("li")
+            info.innerText = data.Transport[0].company
+            comp.appendChild(info)
+
+
+        })
+})
+
+let service = document.getElementById("ser");
+service.addEventListener("click", (e) => {
+    e.preventDefault
+    fetch("https://georgekinyanjui1.github.io/API/db.json")
+        .then((resp) => resp.json())
+        .then((data) => {
+            let satisfied = document.createElement("li")
+            satisfied.innerText = data.Transport[0].services
+            service.appendChild(satisfied)
+
+        })
+})
